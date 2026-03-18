330e6cd0a528c2539ef0561feeaff065cd1d8a79
Anicetagit45 <Aniceta.macamo@gmail.com>
2026-03-18 10:00:13 +0100

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.db import models
 from unfold.admin import ModelAdmin, StackedInline
+from django.db.models import Prefetch
 # REMOVA ou modifique esta linha:
 # from django import forms
 
